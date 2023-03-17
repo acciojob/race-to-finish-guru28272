e988b4daaa1ecf0f0a186efb7724d44f9466c6d8
guru28272 <117896953+guru28272@users.noreply.github.com>
2023-03-17 15:20:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,55 @@
 window.promises = [];
+const output = document.querySelector(`#output`);
+
+function promise1(params) {
+  let myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`promise 1`);
+    }, 3000);
+  });
+  return myPromise;
+}
+function promise2(params) {
+  let myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`promise 2`);
+    }, 2000);
+  });
+  return myPromise;
+}
+
+function promise3(params) {
+  let myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`promise 3`);
+    }, 5000);
+  });
+  return myPromise;
+}
+
+function promise4(params) {
+  let myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`promise 4`);
+    }, 1000);
+  });
+  return myPromise;
+}
+
+function promise5(params) {
+  let myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`promise 5`);
+    }, 1500);
+  });
+  return myPromise;
+}
+
+Promise.any([promise1(), promise2(), promise3(), promise4(), promise5()]).then(
+  (res) => {
+    output.innerHTML = res;
+  }
+);
 
 // Do not change the code above this
 // add your promises to the array `promises`
